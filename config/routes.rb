395d2a92de0bdde1bb69ce9d6e95954dfc92556d
The Mini John <admin@phoenix-prime.gr>
2013-07-29 00:22:24 +0300

    User Profile Page v.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Vaultx::Application.routes.draw do
 
+  get "user/show"
 	root :to => "listings#index"
 	get "about" => "pages#about"
 
 	resources :listings
 
   devise_for :users
+	get 'users/:id' => 'user#show', as: :user
 
 end
