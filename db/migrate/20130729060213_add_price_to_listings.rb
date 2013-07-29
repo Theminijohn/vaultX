deb8db55a9fe0b38d1134e7f07a0bfc9b5245cd1
The Mini John <admin@phoenix-prime.gr>
2013-07-29 09:22:38 +0300

    Price fix

--- db/migrate/20130729060213_add_price_to_listings.rb
+++ db/migrate/20130729060213_add_price_to_listings.rb
@@ -1,5 +1,5 @@
 class AddPriceToListings < ActiveRecord::Migration
   def change
-    add_column :listings, :price, :precision => 8, :scale => 2
+    add_column :listings, :price, :decimal, :precision => 8, :scale => 2
   end
 end
